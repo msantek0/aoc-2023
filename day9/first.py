be81d1d36974851732256aa1a101909479200016
Matija <msantek3@icloud.com>
2023-12-14 20:07:16 +0100

    Day 9 solved

--- day9/first.py
+++ day9/first.py
@@ -1,6 +1,14 @@
 import os, re
 
-TEST = True
+def solve(arr):
+    if all(element == arr[0] for element in arr): return arr[0]
+    new_arr = []
+    for i in range(len(arr)-1):
+        new_arr.append(arr[i+1] - arr[i])
+    solved = solve(new_arr)
+    return arr[-1] + solved
+
+TEST = False 
 
 text_file = "example1.txt" if TEST else "input.txt"
 
@@ -9,3 +17,6 @@ result = 0
 with open(text_file, "r") as f:
     for line in f:
         line = line.strip() 
+        result += solve([int(x) for x in line.split(" ")])
+        
+print(result)
--- day9/second.py
+++ day9/second.py
@@ -1,11 +1,22 @@
 import os, re
 
-TEST = True
+def solve(arr):
+    if all(element == arr[0] for element in arr): return arr[0]
+    new_arr = []
+    for i in range(len(arr)-1):
+        new_arr.append(arr[i+1] - arr[i])
+    solved = solve(new_arr)
+    return arr[0] - solved
 
-text_file = "example2.txt" if TEST else "input.txt"
+TEST = False 
+
+text_file = "example1.txt" if TEST else "input.txt"
 
 result = 0
 
 with open(text_file, "r") as f:
     for line in f:
         line = line.strip() 
+        result += solve([int(x) for x in line.split(" ")])
+        
+print(result)
